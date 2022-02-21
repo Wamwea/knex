2001810adce974bcfa498da0645f83bb12837b8c
Wamwea <mynameiswamwea@gmail.com>
2022-02-21 12:04:25 +0300

    server fixes

--- api/server.js
+++ api/server.js
@@ -1,12 +1,17 @@
 const express = require('express')
-const server = express()
 const lessonsRouter = require('../routes/lessons-routes')
 const messagesRouter = require('../routes/messages-router')
 
+const server = express()
+
 
 server.use(express.json())
-server.use(lessonsRouter)
 server.use('/api/messages', messagesRouter)
 server.use('/api/lessons', lessonsRouter)
+
+
+server.get('/',(req,res)=>{
+    res.status(200).json({message: 'Congrats. Your app has been deployed to heroku!'})
+})
 
 module.exports = server
--- app.js
+++ app.js
@@ -6,7 +6,3 @@ const server = require('./api/server')
 const port = process.env.PORT || 5000
 
 server.listen(port,()=> console.log('Listening on PORT ',port))
-
-server.get('/',(req,res)=>{
-    res.status(200).json({message: 'Congrats. Your app has been deployed to heroku!'})
-})
